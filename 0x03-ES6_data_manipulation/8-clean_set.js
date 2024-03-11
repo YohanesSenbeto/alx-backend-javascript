9cb9e325d909319cbcd671381a710bab3cf862ec
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-03-11 16:47:30 +0300

    ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+function cleanSet(set, startString) {
+	    const filteredValues = [...set].filter((value) =>
+		            value.startsWith(startString)
+		        );
+	    const cleanedValues = filteredValues.map((value) =>
+		            value.slice(startString.length)
+		        );
+
+	    return cleanedValues.join("-");
+}
+
+export default cleanSet;
