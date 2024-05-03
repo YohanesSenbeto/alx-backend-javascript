f703ab1ea265260d8863b6d25384b3a42e8acc02
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-05-03 11:30:21 +0000

    Test file for console.js done

---
+++ 0x05-Node_JS_basic/0-console.test.js
@@ -0,0 +1,12 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const displayMessage = require('./0-console');
+
+describe('displayMessage', () => {
+  it('prints the message to the console', () => {
+    const consoleLogSpy = sinon.spy(console, 'log');
+    displayMessage('Hello NodeJS!');
+    expect(consoleLogSpy.calledWith('Hello NodeJS!')).to.be.true;
+    consoleLogSpy.restore();
+  });
+});
